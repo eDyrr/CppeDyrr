ca69915b42787e62b7945e6b03230ea46e7b9088
eD <91418532+eDyrr@users.noreply.github.com>
2023-06-11 21:02:58 +0100

    Update InsertionSort.cpp

--- Sort_Algorithms/Insertion_Sort/InsertionSort.cpp
+++ Sort_Algorithms/Insertion_Sort/InsertionSort.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 
-void exchange(int& a, int& b)
+void swap(int& a, int& b)
 {
-    int c = a ; 
-    a = b ; 
-    b = c  ;
+    a = a ^ b ; 
+    b = a ^ b ; 
+    a = a ^ b ; 
 }
 
 void InsertionSort(int array[], int size)
 {
     for(int i = 0 ; i < size ; i++)
         for(int j = i ; j > -1 ; j--)
-            if(array[j] < array[j-1]) exchange(array[j] , array[j-1]) ; 
+            if(array[j] < array[j-1]) swap(array[j] , array[j-1]) ; 
             else break ; 
 }
 
